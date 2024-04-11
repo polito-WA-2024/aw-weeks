70491669d549493d7177b7ce87763bc0976dba0b
Enrico Masala <enrico.masala@polito.it>
2024-04-11 15:33:49 +0200

    Intermediate version of app

--- week05/js_browser/app.js
+++ week05/js_browser/app.js
@@ -35,3 +35,56 @@ answerList.forEach(e => console.log(e.str()));
 // Populate the list in the HTML ...
 // ...
 
+function createAnswerNode(ans) {
+
+    const newTd1 = document.createElement("td");
+    const newContentDate = document.createTextNode(ans.date.format('YYYY-MM-DD'));
+    newTd1.appendChild(newContentDate);
+
+    const newTd2 = document.createElement("td");
+    const newContentText = document.createTextNode(ans.text);
+    newTd2.appendChild(newContentText);
+    
+    const newTd3 = document.createElement("td");
+    const newContentRespondent = document.createTextNode(ans.respondent);
+    newTd3.appendChild(newContentRespondent);
+    
+    const newTd4 = document.createElement("td");
+    const newContentScore = document.createTextNode(ans.score);
+    newTd4.appendChild(newContentScore);
+
+    const newTd5 = document.createElement("td");
+    const newButton = document.createElement("button");
+    newButton.className = "btn btn-primary";
+    newButton.id = ans.id;
+    newButton.textContent = "Vote";
+    newTd5.appendChild(newButton);
+
+    newButton.addEventListener('click', event => {
+        //console.log(event.target);
+        vote(event.target.id);
+    });
+
+    const newTr = document.createElement("tr");
+    newTr.appendChild(newTd1);
+    newTr.appendChild(newTd2);
+    newTr.appendChild(newTd3);
+    newTr.appendChild(newTd4);
+    newTr.appendChild(newTd5);
+
+    return newTr;
+
+}
+
+
+
+function createAnswerList(answerList) {
+    //const tableBody = document.querySelector('tbody');
+    const tableBody = document.getElementById('answers');
+    for (let ans of answerList) {
+        const newRow = createAnswerNode(ans);
+        tableBody.appendChild(newRow);
+    }
+}
+
+createAnswerList(answerList);
