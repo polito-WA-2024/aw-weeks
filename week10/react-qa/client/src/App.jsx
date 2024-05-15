7e39a992df90f4448f5353e3771cf2567c9d42f5
Enrico Masala <enrico.masala@polito.it>
2024-05-15 11:08:44 +0200

    Initial example for the lecture

---
+++ week10/react-qa/client/src/App.jsx
@@ -0,0 +1,154 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import { useEffect, useState } from 'react';
+import { Col, Container, Row, Navbar, Button } from 'react-bootstrap';
+import { BrowserRouter, Routes, Route, Outlet, Link } from 'react-router-dom'; 
+import './App.css';
+
+import { AnswerTable } from './components/AnswerComponents.jsx';
+import { QuestionDescription } from './components/QuestionComponents.jsx';
+import { FormRoute } from './components/FormComponents.jsx';
+
+import { Question } from './QAModels.js';
+
+const initialQuestion = new Question(1, 'Best way of enumerating an array in JS?', 'Enrico', '2024-03-01');
+initialQuestion.init();
+const initialAnswerList = initialQuestion.getAnswers();
+
+
+function MyHeader(props) {
+	return (
+		<Navbar bg="primary" variant="dark">
+      <Navbar.Brand className="mx-2">
+      <i className="bi bi-collection-play" />
+      {/* props.appName just in case you want to set a different app name */}
+			{props.appName || "HeapOverrun"}
+      </Navbar.Brand>
+		</Navbar>
+	);
+}
+
+
+function MyFooter(props) {
+  return (<footer>
+    <p>&copy; Web Applications</p>
+    <div id="time"></div>
+  </footer>);
+}
+
+
+function AnswerRoute(props) { 
+
+  return (<>
+    <Row>
+      <QuestionDescription question={props.question} />
+    </Row>
+    <Row>
+      <Col>
+        <h2>Current Answers</h2>
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+        <AnswerTable listOfAnswers={props.answerList} vote={props.voteAnswer} delete={props.deleteAnswer} />
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+        <Link to='/add'> 
+          <Button>Add</Button> 
+        </Link>
+      </Col>
+    </Row>
+  </>
+  );
+}
+
+function DefaultRoute(props) {
+  return (
+    <Container fluid>
+      <p className="my-2">No data here: This is not a valid page!</p>
+      <Link to='/'>Please go back to main page</Link>
+    </Container>
+  );
+}
+
+function App() {
+  // state moved up into App
+  const [ question, setQuestion ] = useState(initialQuestion);
+  const [ answers, setAnswers ] = useState(initialAnswerList);
+
+
+  function voteAnswer(id, delta) {
+    setAnswers( answerList => 
+      answerList.map(e => e.id === id ? Object.assign({}, e, {score: e.score+delta}) : e)
+    );
+  }
+
+  function deleteAnswer(id) {
+    setAnswers( answerList =>
+      answerList.filter(e => e.id !== id)
+    );
+  }
+
+  function addAnswer(answer) {
+    setAnswers( answerList => {
+      // NB: max does not take an array but a set of parameters
+      const newId = Math.max(...answerList.map(e => e.id))+1;
+      answer.id = newId;
+      return [...answerList, answer];
+    }
+    );
+  //setShowForm(false);
+  }
+
+  function saveExistingAnswer(answer) {
+    setAnswers( answerList => 
+      answerList.map( e => e.id === answer.id ? answer : e)
+    );
+    //setShowForm(false);
+    //setEditObj(undefined);
+  }
+
+/*
+  function setEditAnswer(id) {
+    setEditObj( answers.find( e => e.id === id) );
+    setShowForm(true);
+  }
+*/
+
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+          <Route index element={ <AnswerRoute question={question} answerList={answers}
+            voteAnswer={voteAnswer} deleteAnswer={deleteAnswer} /> } />
+          <Route path='/add' element={ <FormRoute addAnswer={addAnswer} /> } />
+          <Route path='/edit/:answerId' element={<FormRoute answerList={answers}
+            addAnswer={addAnswer} editAnswer={saveExistingAnswer} />} />
+      </Route>
+      <Route path='/*' element={<DefaultRoute />} />
+    </Routes>
+  </BrowserRouter>
+  );
+}
+
+function Layout(props) {
+return (
+<Container fluid>
+      <Row>
+        <Col>
+          <MyHeader />
+        </Col>
+      </Row>
+      <Outlet />
+      <Row>
+        <Col>
+          <MyFooter />
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default App
---
+++ week10/react-qa/server/index.js
@@ -0,0 +1,196 @@
+'use strict';
+
+const express = require('express');
+const morgan = require('morgan'); // logging middleware
+const {check, validationResult} = require('express-validator'); // validation middleware
+const dao = require('./dao'); // module for accessing the DB.  NB: use ./ syntax for files in the same dir
+
+// init express
+const app = express();
+const port = 3001;
+
+// set-up the middlewares
+app.use(morgan('dev'));
+app.use(express.json()); // To automatically decode incoming json
+
+
+/*** APIs ***/
+
+// GET /api/questions
+app.get('/api/questions', (req, res) => {
+  dao.listQuestions()
+    .then(questions => res.json(questions))
+    .catch(() => res.status(500).end());
+});
+
+// GET /api/questions/<id>
+app.get('/api/questions/:id', async (req, res) => {
+  try {
+    const result = await dao.getQuestion(req.params.id);
+    if(result.error)
+      res.status(404).json(result);
+    else
+      res.json(result);
+  } catch(err) {
+    res.status(500).end();
+  }
+});
+
+// GET /api/questions/<id>/answers
+app.get('/api/questions/:id/answers', async (req, res) => {
+  try {
+    const resultQuestion = await dao.getQuestion(req.params.id);
+    //console.log('Question: ' + JSON.stringify(resultQuestion));
+    if (resultQuestion.error)
+      res.status(404).json(resultQuestion);   // questionId does not exist
+    else {
+      const result = await dao.listAnswersByQuestion(req.params.id);
+      //console.log("result: "+JSON.stringify(result));
+      //console.log("length: "+result.length);
+      if (result.error)
+        res.status(404).json(result);
+      else
+        res.json(result);  // NB: list of answers can also be an empty array
+    }
+  } catch(err) {
+    res.status(500).end();
+  }
+});
+
+
+// GET /api/answers
+app.get('/api/answers', async (req, res) => {
+  try {
+    const answers = await dao.listAnswers();
+    res.json(answers);
+  } catch(err) {
+    res.status(500).end();
+  }
+});
+
+// GET /api/answers/<id>
+app.get('/api/answers/:id', async (req, res) => {
+  try {
+    const result = await dao.getAnswer(req.params.id);
+    if(result.error)
+      res.status(404).json(result);
+    else
+      res.json(result);
+  } catch(err) {
+    res.status(500).end();
+  }
+});
+
+
+// POST /api/answers
+app.post('/api/answers', [
+  check('score').isInt(),
+  check('respondent').isLength({min: 1}),   // as an example
+  check('date').isDate({format: 'YYYY-MM-DD', strictMode: true})
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({errors: errors.array()});
+  }
+
+  const questionId = req.body.questionId;
+  const resultQuestion = await dao.getQuestion(questionId);  // db consistency: make sure questionId already exists
+  if (resultQuestion.error)
+    res.status(404).json(resultQuestion);   // questionId does not exist, please insert the question before the answer
+  else {
+    const answer = {
+      questionId: questionId,
+      score: req.body.score,
+      date: req.body.date,
+      text: req.body.text,
+      respondent: req.body.respondent,
+    };
+
+    try {
+      const newAnswer = await dao.createAnswer(answer);
+      res.status(201).json(newAnswer);
+    } catch (err) {
+      res.status(503).json({ error: `Database error during the creation of answer ${answer.text} by ${answer.respondent}.` });
+    }
+  }
+});
+
+
+// PUT /api/answers/<id>
+app.put('/api/answers/:id', [
+  check('score').isInt(),
+  check('respondent').isLength({min: 1}),   // as an example
+  check('date').isDate({format: 'YYYY-MM-DD', strictMode: true}),
+  check('id').isInt()
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({errors: errors.array()});
+  }
+
+  const answer = req.body;
+  // you can also check here if the id passed in the URL matches with the id in req.body,
+  // and decide which one must prevail, or return an error
+  answer.id = req.params.id;
+
+  try {
+    const numRowChanges = await dao.updateAnswer(answer);
+    res.json(numRowChanges);
+    //res.status(200).end();
+  } catch(err) {
+    res.status(503).json({error: `Database error during the update of answer ${req.params.id}.`});
+  }
+
+});
+
+
+// POST /api/answers/<id>/vote
+// NOTE: this is a POST, not a PUT, since it is NOT idempotent
+app.post('/api/answers/:id/vote', [
+  check('id').isInt(),
+  check('vote').isIn(['upvote','downvote'])
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({errors: errors.array()});
+  }
+
+  try {
+    const numRowChanges = await dao.voteAnswer(req.params.id, req.body.vote);
+    // number of changed rows is sent to client as an indicator of success
+    res.json(numRowChanges);
+  } catch (err) {
+    res.status(503).json({ error: `Database error while voting answer ${req.params.id}.` });
+  }
+
+});
+
+
+// DELETE /api/answers/<id>
+app.delete('/api/answers/:id', [
+  check('id').isInt()
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({errors: errors.array()});
+  }
+  try {
+    const numRowChanges = await dao.deleteAnswer(req.params.id);  
+    // NOTE: if there is no element with the specified id, the delete operation is considered successful
+    // since the final status of the server is that the element with that id does not exist.
+    // This is also consistent with the fact that DELETE should be idempotent.
+    // However, for easier debugging, we send the number of affected (changed) rows to the client.
+    res.json(numRowChanges);
+  } catch(err) {
+    console.log(err);
+    res.status(503).json({ error: `Database error during the deletion of answer ${req.params.id}.`});
+  }
+});
+
+
+/*** Other express-related instructions ***/
+
+// Activate the server
+app.listen(port, () => {
+  console.log(`qa-server listening at http://localhost:${port}`);
+});
